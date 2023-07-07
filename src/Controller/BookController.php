d7ee3993e253757c469feee1fd147ce13140a186
anjaNante <36764520+anjanante@users.noreply.github.com>
2023-07-07 14:52:16 +0300

    Create book without Author

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,8 +7,10 @@ use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
@@ -34,5 +36,16 @@ class BookController extends AbstractController
         $em->remove($book);
         $em->flush();
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/books', name: 'createBook', methods:['POST'])]
+    public function createBook(Request $request, EntityManagerInterface $em, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        $book   = $serializer->deserialize($request->getContent(), Book::class, 'json');
+        $em->persist($book);
+        $em->flush();
+        $jsonBook   = $serializer->serialize($book,'json', ['groups' => 'getBooks']);
+        $location   = $urlGenerator->generate('detailBook', ['id' => $book->getid()], UrlGeneratorInterface::ABSOLUTE_PATH);
+        return new JsonResponse($jsonBook, Response::HTTP_CREATED, ['Location' => $location], true);
     }
 }
